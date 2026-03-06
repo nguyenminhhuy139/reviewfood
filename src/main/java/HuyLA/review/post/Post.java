b454dc8ef2745316a8144d7d60bd46e48344c9ab
Lan Anh <trantruonglananh225@gmail.com>
2026-03-06 16:58:34 +0700

    first commit

---
+++ src/main/java/HuyLA/review/post/Post.java
@@ -0,0 +1,32 @@
+package HuyLA.review.post;
+
+import HuyLA.review.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String imageUrl;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private User author;
+}
