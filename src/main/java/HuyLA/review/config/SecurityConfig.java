af53d1ec77a77e3f2cc7b73c23c30987b01315b7
Nguyen Minh Huy <kutosuper@gmail.com>
2026-04-10 16:20:57 +0700

    add GET api/posts no auth

--- src/main/java/HuyLA/review/config/SecurityConfig.java
+++ src/main/java/HuyLA/review/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .cors(cors -> {})
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("GET", "/api/posts").permitAll()
                         .anyRequest().authenticated()
                 );
 
